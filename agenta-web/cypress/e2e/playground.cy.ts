48ee8103243c93b583f043040d44e6c62131af5c
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-12-08 12:48:21 +0100

    modifies playground test

--- agenta-web/cypress/e2e/playground.cy.ts
+++ agenta-web/cypress/e2e/playground.cy.ts
@@ -4,14 +4,11 @@ describe("Playground Prompt Test", function () {
     })
 
     it("Should test prompt functionality in the Playground", () => {
-        cy.get('[data-cy^="testview-input-parameters"]').eq(0).type("Germany")
+        cy.get('[data-cy^="testview-input-parameters-"]').eq(0).type("Germany")
         cy.get('[data-cy="testview-input-parameters-run-button"]').click()
+        cy.get('[data-cy="testview-input-parameters-result"]').should("have.value", "Loading...")
         cy.get('[data-cy="testview-input-parameters-result"]').should(
-            "contain.text",
-            "Loading...",
-        )
-        cy.get('[data-cy="testview-input-parameters-result"]').should(
-            "contain.text",
+            "have.value",
             "The capital of Germany is Berlin.",
         )
         cy.get(".ant-message-notice-content").should("not.exist")
